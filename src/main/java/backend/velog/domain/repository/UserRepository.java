44b080357c2bbb8dff91b0d371027599be6436ae
jongwanhong <infinity4869@gmail.com>
2021-04-07 00:14:35 +0900

    add jwt

--- src/main/java/backend/velog/domain/repository/UserRepository.java
+++ src/main/java/backend/velog/domain/repository/UserRepository.java
@@ -3,5 +3,9 @@ package backend.velog.domain.repository;
 import backend.velog.domain.model.User;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.Optional;
+
 public interface UserRepository extends JpaRepository<User, Long> {
+
+    Optional<User> findByNickname(String nickname);
 }
