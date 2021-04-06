efd5a93736cb9ae61c0c2088682844b119795555
jongwanhong <infinity4869@gmail.com>
2021-04-06 23:06:01 +0900

    add base login

--- src/main/java/backend/velog/domain/repository/UserRepository.java
+++ src/main/java/backend/velog/domain/repository/UserRepository.java
@@ -1,4 +1,7 @@
 package backend.velog.domain.repository;
 
-public interface UserRepository {
+import backend.velog.domain.model.User;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface UserRepository extends JpaRepository<User, Long> {
 }
