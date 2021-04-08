c63719e2ed233bb57a31286ad0edd753ad5b8df8
jongwanhong <infinity4869@gmail.com>
2021-04-08 17:20:02 +0900

    cors allow port 3000

--- src/main/java/backend/velog/controller/HomeController.java
+++ src/main/java/backend/velog/controller/HomeController.java
@@ -4,7 +4,6 @@ import backend.velog.domain.model.Board;
 import backend.velog.domain.repository.BoardRepository;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Controller;
-import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 
 import java.util.List;
@@ -19,25 +18,6 @@ public class HomeController {
     @GetMapping("/api/home")
     public List<Board> testHome() {
         return boardRepository.findAllByOrderByCreatedAtDesc();
-    }
-
-    // 회원 로그인 페이지
-    @GetMapping("/login")
-    public String login() {
-        return "login";
-    }
-
-    // 에러 페이지
-    @GetMapping("/login/error")
-    public String loginError(Model model) {
-        model.addAttribute("loginError", true);
-        return "login";
-    }
-
-    // 회원 가입 페이지
-    @GetMapping("/signup")
-    public String signup() {
-        return "signup";
     }
 
 }
